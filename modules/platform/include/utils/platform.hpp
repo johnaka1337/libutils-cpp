bd5df64e91774c03d4da5c604f304e78196b5e04
John Crew <johnaka1337@gmail.com>
2026-03-04 07:58:42 +0500

    feat(platform): add core fixed-width types and umbrella header

---
+++ modules/platform/include/utils/platform.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "utils/platform/types.hpp"
+
+
+
+
+namespace utils::platform
+{
+
+}
---
+++ modules/platform/include/utils/platform/types.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+
+
+
+namespace utils::platform
+{
+using std::size_t;
+
+using std::int32_t;
+using std::uint32_t;
+
+using std::int64_t;
+using std::uint64_t;
+}
